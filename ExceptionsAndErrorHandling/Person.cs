1c8b3b5e2e74c506552fc9b8eff7dbd94f15d3e4
karimmoustamid <karim.moustamid@agilethought.com>
2024-10-03 19:37:30 -0600

    Add null check for name parameter in Person constructor
    This commit adds a check to throw an ArgumentNullException if the 'name' parameter is null, improving the robustness of the constructor. Additionally, it changes the exception thrown for an invalid year of birth from AggregateException to ArgumentOutOfRangeException to more accurately represent the error.

--- ExceptionsAndErrorHandling/Person.cs
+++ ExceptionsAndErrorHandling/Person.cs
@@ -7,6 +7,11 @@ namespace ExceptionsAndErrorHandling
 
         public Person(string name, int yearOfBirth)
         {
+            if (name is null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
             if (name == string.Empty)
             {
                 throw new ArgumentException("Name cannot be empty",
@@ -15,7 +20,7 @@ namespace ExceptionsAndErrorHandling
 
             if (yearOfBirth < 1900 || yearOfBirth > DateTime.Now.Year)
             {
-                throw new AggregateException("Invalid year of birth");
+                throw new ArgumentOutOfRangeException("Invalid year of birth");
             }
         }
     }
